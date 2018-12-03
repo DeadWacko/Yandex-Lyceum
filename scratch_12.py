1895762038b4489fc6804da7d6ec68e0864e0d88
DeadWacko <38377417+DeadWacko@users.noreply.github.com>
2018-12-03 19:09:15 +0300

    Add files via upload

--- scratch_12.py
+++ scratch_12.py
@@ -1,15 +1,24 @@
 col_1=int(input())
 col_2=int(input())
+col_3=int(input())
 mn_1=set()
 mn_2=set()
-for i in range(col_1+col_2):
+mn_3=set()
+n = 0
+for i in range(col_1+col_2+col_3):
     st = str(input())
     if st in mn_1:
-        mn_2.add(st)
+        if st in mn_2:
+            mn_3.add(st)
+            mn_2.remove(st)
+        else:
+            mn_2.add(st)
     else:
         mn_1.add(st)
 
-if len(mn_1 ^ mn_2) == 0:
-    print('NO')
+n = len(mn_2)
+
+if n != 0:
+    print(n)
 else:
-    print(len(mn_1 ^ mn_2))
+    print('NO')
